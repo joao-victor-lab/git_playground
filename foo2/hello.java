06111e27db804ef9532574f1c5f876dd0892076b
joao-victor-lab <joaosan17017vic@gmail.com>
2023-10-14 16:54:41 -0300

    segue a luta do sing
    Signed-off-by: joao-victor-lab <joaosan17017vic@gmail.com>

--- foo2/hello.java
+++ foo2/hello.java
@@ -1,5 +1,11 @@
 public class hello {
   public static void main(String[] args) {
     System.out.println("code....... end nubsss.");
+
+    int a,b;
+    public int sum(int a; int b;) {
+      return a + b;
+    }
+    
   }
 }
