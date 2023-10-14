37ffcc5e0fa406842189a5de4aa0002d75826a20
joao-victor-lab <79527341+joao-victor-lab@users.noreply.github.com>
2023-10-14 17:27:31 -0300

    change for pattern java name Class
    Signed-off-by: joao-victor-lab <79527341+joao-victor-lab@users.noreply.github.com>

---
+++ foo2/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+  int a;
+  int b;
+  static int sum(int a , int b) {
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sum(2,2));
+  }
+}
